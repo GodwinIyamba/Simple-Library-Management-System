a86dd3d91bb3b926344c1bdcf340ce22a1e827ae
) Iyamba <mac@MACs-MBP.lan>
2023-01-05 10:31:13 +0100

    Add book details link to Book title and image

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -17,10 +17,10 @@
                             <div class="product col-lg-2 col-md-3 col-6 px-2">
                                 <div class="grid-inner">
                                     <div class="product-image">
-                                        <a href="#"><img src="{{ asset($book->book_cover)}}" alt="Checked Short Dress"></a>
+                                        <a href="{{ route('book_details', $book) }}"><img src="{{ asset($book->book_cover)}}" alt="Checked Short Dress"></a>
                                     </div>
                                     <div class="product-desc">
-                                        <div class="mb-0"><h4 class="mb-2" style="font-size: 0.9rem;"><a href="#">{{ $book->title }}</a></h4></div>
+                                        <div class="mb-0"><h4 class="mb-2" style="font-size: 0.9rem;"><a href="{{ route('book_details', $book) }}">{{ $book->title }}</a></h4></div>
                                         <div class="product-price"><h5 class="mb-1">{{ $book->author->name }}</h5></div>
                                         <div>
                                             <a href="{{ route('book_details', $book) }}" class="button button-3d button-mini button-rounded button-black">LEARN MORE</a>
